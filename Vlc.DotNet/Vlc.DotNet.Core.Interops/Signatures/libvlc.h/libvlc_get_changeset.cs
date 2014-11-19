4b4ee287a1c4398f35c3be99599473f38b6262ff
ZeBobo5 <zebobo5@hotmail.com>
2014-11-19 15:50:41 +0100

    Add libvlc_get_changeset signature

---
+++ Vlc.DotNet/Vlc.DotNet.Core.Interops/Signatures/libvlc.h/libvlc_get_changeset.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Vlc.DotNet.Core.Interops.Signatures
+{
+    /// <summary>
+    /// Retrieve libvlc changeset.
+    /// </summary>
+    /// <returns>Return a string containing the libvlc changeset.</returns>
+    [LibVlcFunction("libvlc_get_changeset")]
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    internal delegate IntPtr GetChangeset();
+}
---
+++ Vlc.DotNet/Vlc.DotNet.Core.Interops/VlcManager.GetChangeset.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Vlc.DotNet.Core.Interops.Signatures;
+
+namespace Vlc.DotNet.Core.Interops
+{
+    public sealed partial class VlcManager
+    {
+        public string GetChangeset()
+        {
+#if !NET20
+            return GetInteropDelegate<GetChangeset>().Invoke().ToStringAnsi();
+#else
+            return IntPtrExtensions.ToStringAnsi(GetInteropDelegate<GetChangeset>().Invoke());
+#endif
+        }
+    }
+}
