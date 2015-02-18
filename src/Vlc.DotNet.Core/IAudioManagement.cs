14901aa911f9eb0ae3e1a7ef9267c1022c9e4606
ZeBobo5 <zebobo5@hotmail.com>
2015-02-18 10:51:36 +0100

    Add Audio properties to IAudiomanagement

--- src/Vlc.DotNet.Core/IAudioManagement.cs
+++ src/Vlc.DotNet.Core/IAudioManagement.cs
@@ -3,5 +3,17 @@
     public interface IAudioManagement
     {
         IAudioOutputsManagement Outputs { get; }
+
+        bool IsMute { get; set; }
+
+        void ToggleMute();
+
+        int Volume { get; set; }
+
+        ITracksManagement Tracks { get; }
+
+        int Channel { get; set; }
+
+        long Delay { get; set; }
     }
 }
