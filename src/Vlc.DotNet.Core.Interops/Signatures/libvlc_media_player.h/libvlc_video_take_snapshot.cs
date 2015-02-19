a948acc62506a329ac5513cae1f58df5b021b208
ZeBobo5 <zebobo5@hotmail.com>
2015-02-19 16:09:44 +0100

    Fixes #11 - WPF Control can't take screenshot

---
+++ src/Vlc.DotNet.Core.Interops/Signatures/libvlc_media_player.h/libvlc_video_take_snapshot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Vlc.DotNet.Core.Interops.Signatures
+{
+    /// <summary>
+    /// Take a snapshot of the current video window.
+    /// </summary>
+    /// <returns>Return 0 on success, -1 if the video was not found.</returns>
+    [LibVlcFunction("libvlc_video_take_snapshot")]
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    internal delegate int TakeSnapshot(IntPtr mediaPlayerInstance, uint num, string fileName, uint width, uint height);
+}
