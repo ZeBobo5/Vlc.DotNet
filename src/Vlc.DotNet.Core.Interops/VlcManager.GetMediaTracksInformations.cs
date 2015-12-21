baf3f4765d51eba58c5501c15b7036ad87e4a5a4
ZeBobo5 <zebobo5@hotmail.com>
2015-12-21 12:51:22 -1000

    Fixed ToInt32() call OverflowException crash with VLC 64 bits

--- src/Vlc.DotNet.Core.Interops/VlcManager.GetMediaTracksInformations.cs
+++ src/Vlc.DotNet.Core.Interops/VlcManager.GetMediaTracksInformations.cs
@@ -19,7 +19,11 @@ namespace Vlc.DotNet.Core.Interops
             for (int index = 0; index < cpt; index++)
             {
                 result[index] = (MediaTrackInfosStructure)Marshal.PtrToStructure(buffer, typeof(MediaTrackInfosStructure));
+#if X86
                 buffer = new IntPtr(buffer.ToInt32() + Marshal.SizeOf(typeof(MediaTrackInfosStructure)));
+#else
+                buffer = new IntPtr(buffer.ToInt64() + Marshal.SizeOf(typeof(MediaTrackInfosStructure)));
+#endif
             }
             Free(fullBuffer);
             return result;
