ea813f145a5d30c3771e27c989d83150c70cfbf7
Massimo Savazzi <massimo@savazzi.eu>
2016-02-03 22:16:51 +0100

    Added dynamic objects example in VB.NET Works both in x64 and x86

---
+++ src/Samples/Vlc.DotNet.Forms.Samples/DynamicObject/DynamicObject.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class DynamicObject
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(3, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(168, 44)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Create Object"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(781, 446)
+        Me.Panel1.TabIndex = 1
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(4, 53)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(167, 44)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Play File"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.SplitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
+        Me.SplitContainer1.Location = New System.Drawing.Point(606, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Button3)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Button1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label2)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(175, 446)
+        Me.SplitContainer1.SplitterDistance = 102
+        Me.SplitContainer1.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(781, 446)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button3 As Button
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ src/Samples/Vlc.DotNet.Forms.Samples/DynamicObject/DynamicObject.vb
@@ -0,0 +1,97 @@
+﻿Imports System.IO
+Imports Vlc.DotNet
+
+Public Class DynamicObject
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim b As Forms.VlcControl
+        Dim aP As String
+        b = New Vlc.DotNet.Forms.VlcControl
+
+        b.BeginInit()
+        If Environment.Is64BitOperatingSystem Then
+            If Environment.Is64BitProcess Then
+                aP = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "VideoLAN\VLC")
+            Else
+                aP = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "VideoLAN\VLC")
+            End If
+        Else
+            aP = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "VideoLAN\VLC")
+        End If
+        b.VlcLibDirectory = New DirectoryInfo(aP)
+        b.Parent = Panel1
+        b.Dock = DockStyle.Fill
+        AddHandler b.VlcLibDirectoryNeeded, AddressOf checkdir
+        AddHandler b.Playing, AddressOf onPlay2
+
+        Panel1.Controls.Add(b)
+
+        b.EndInit()
+    End Sub
+
+    Private Function checkdir() As DirectoryInfo
+        Dim aP As String
+
+        If Environment.Is64BitOperatingSystem Then
+            If Environment.Is64BitProcess Then
+                aP = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "VideoLAN\VLC")
+            Else
+                aP = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "VideoLAN\VLC")
+            End If
+        Else
+            aP = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "VideoLAN\VLC")
+        End If
+        If Not File.Exists(Path.Combine(aP, "libvlc.dll")) Then
+            Using fbdDialog As New FolderBrowserDialog()
+                fbdDialog.Description = "Select VLC Path"
+                fbdDialog.SelectedPath = Path.Combine(aP, "VideoLAN\VLC")
+
+                If fbdDialog.ShowDialog() = DialogResult.OK Then
+                    Return New DirectoryInfo(fbdDialog.SelectedPath)
+                End If
+            End Using
+        Else
+            Return New DirectoryInfo(aP)
+        End If
+        Return Nothing
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim aP As Vlc.DotNet.Forms.VlcControl
+        Using fbdDialog As New OpenFileDialog()
+            fbdDialog.Title = "Select video"
+            fbdDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos)
+            fbdDialog.Filter = "All files (*.*)|*.*"
+            fbdDialog.RestoreDirectory = True
+            fbdDialog.Multiselect = False
+
+            If fbdDialog.ShowDialog() = DialogResult.OK Then
+                aP = CType(Panel1.Controls(0), Vlc.DotNet.Forms.VlcControl)
+                aP.Play(New Uri(fbdDialog.FileName))
+            End If
+        End Using
+
+    End Sub
+
+    Private Sub onPlay2()
+        Dim aP As Vlc.DotNet.Forms.VlcControl
+        aP = CType(Panel1.Controls(0), Vlc.DotNet.Forms.VlcControl)
+        Dim aP2 As String = aP.GetCurrentMedia.URL
+        If Me.InvokeRequired Then
+            Me.Invoke(Sub() onPlay2())
+            Return
+        End If
+        For Each aM In aP.GetCurrentMedia().TracksInformations
+            If aM.Type = Core.Interops.Signatures.MediaTrackTypes.Audio Then
+                Label1.Text = aM.CodecName
+            Else
+                If aM.Type = Core.Interops.Signatures.MediaTrackTypes.Video Then
+                    Label2.Text = aM.CodecName
+                End If
+            End If
+        Next
+    End Sub
+End Class
