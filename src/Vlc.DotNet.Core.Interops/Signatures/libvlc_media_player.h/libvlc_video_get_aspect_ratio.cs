4eb2df348367db5173d5a67bb73b5d2df94d40b6
Timothy <microchi@gmail.com>
2016-03-17 02:23:59 +0800

    Fix Get Aspect Ratio Bug

--- src/Vlc.DotNet.Core.Interops/Signatures/libvlc_media_player.h/libvlc_video_get_aspect_ratio.cs
+++ src/Vlc.DotNet.Core.Interops/Signatures/libvlc_media_player.h/libvlc_video_get_aspect_ratio.cs
@@ -6,7 +6,7 @@ namespace Vlc.DotNet.Core.Interops.Signatures
     /// <summary>
     /// Get current crop filter geometry.
     /// </summary>
-    [LibVlcFunction("libvlc_video_get_crop_geometry")]
+    [LibVlcFunction("libvlc_video_get_aspect_ratio")]
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     internal delegate IntPtr GetVideoAspectRatio(IntPtr mediaPlayerInstance);
 }
