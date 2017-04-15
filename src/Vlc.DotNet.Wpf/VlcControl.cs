49aa8f95f6013a62adde785a081f9ec7ab4101c6
xue-blood <xue-blood@users.noreply.github.com>
2017-04-15 15:53:28 +0800

    Update VlcControl.cs

--- src/Vlc.DotNet.Wpf/VlcControl.cs
+++ src/Vlc.DotNet.Wpf/VlcControl.cs
@@ -108,14 +108,14 @@ namespace Vlc.DotNet.Wpf
         #region property
 
         #region time
-        private Wpf.Time length = 0;
-        public Wpf.Time Length
+        private Time length = 0;
+        public Time Length
         {
             get { return length; }
         }
 
-        private  Wpf.Time time = 0;
-        public  Wpf.Time Time
+        private  Time time = 0;
+        public  Time Time
         {
             get { return time; }
             set
@@ -245,4 +245,76 @@ namespace Vlc.DotNet.Wpf
         }
         #endregion
     }
+    
+
+    public class Time
+    {
+        // time as ms
+        public long time;
+
+        public override string ToString()
+        {
+            return TimeSpan.FromMilliseconds(time).
+                ToString(@"h\:mm\:ss");
+        }
+
+        public Time()
+        {
+            time = 0;
+        }
+
+        public Time(long ms)
+        {
+            time = ms;
+        }
+
+        /*
+         * override compare
+         */
+        public static bool operator ==(Time t1, Time t2)
+        {
+            if (t1.time == t2.time)
+                return true;
+            return false;
+        }
+        public static bool operator !=(Time t1, Time t2)
+        {
+            if (t1.time != t2.time)
+                return true;
+            return false;
+        }
+
+        /* 
+         * implicit convert
+         * refer:http://stackoverflow.com/questions/4537803/overloading-assignment-operator-in-c-sharp
+         */
+        public static implicit operator Time(long value)
+        {
+            return new Time(value);
+        }
+
+        public static implicit operator long(Time value)
+        {
+            if (value.Equals(null))
+                return 0;
+            return value.time; 
+        }
+
+        public static Time Parse(string s)
+        {
+            Time tm = null;
+            try
+            {
+                TimeSpan t = TimeSpan.Parse(s);
+                 tm = (long)t.TotalMilliseconds;
+            }
+            catch (Exception)
+            {
+                tm = 0;
+            }
+            return tm;
+        }
+    }
+}
+
 }
