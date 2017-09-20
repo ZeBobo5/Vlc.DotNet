e205968be481f8cc8ca8bb6b9e32bbcbe9388dc2
Jérémy VIGNELLES <jeremy.vignelles@gmail.com>
2017-09-20 23:13:55 +0200

    fix for dubious UTF8 conversion

--- src/Vlc.DotNet.Core/TrackDescription.cs
+++ src/Vlc.DotNet.Core/TrackDescription.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
+using Vlc.DotNet.Core.Interops;
 using Vlc.DotNet.Core.Interops.Signatures;
 
 namespace Vlc.DotNet.Core
@@ -23,7 +24,7 @@ namespace Vlc.DotNet.Core
             if (moduleRef != IntPtr.Zero)
             {
                 var module = (TrackDescriptionStructure)Marshal.PtrToStructure(moduleRef, typeof(TrackDescriptionStructure));
-                var name = Encoding.UTF8.GetString(Encoding.Default.GetBytes(module.Name));
+                var name = Utf8InteropStringConverter.Utf8InteropToString(module.Name);
                 result.Add(new TrackDescription(module.Id, name));
                 var data = GetSubTrackDescription(module.NextTrackDescription);
                 result.AddRange(data);
