008311a35705084ce0732f5317f08142c2c8b34c
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-09-21 12:06:31 +0200

    Add support for VlcMediaPlayerInstance.SetCallbacks and VlcmediaPlayerInstance.SetFormatCallbacks

---
+++ src/Vlc.DotNet.Core.Interops/Signatures/libvlc_media_player.h/libvlc_video_set_callbacks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Vlc.DotNet.Core.Interops.Signatures
+{
+    /// <summary>
+    /// Set callbacks and private data to render decoded video to a custom area
+    /// in memory.
+    /// Use <see cref="SetVideoFormatCallbacks"/> to configure the decoded format.
+    /// </summary>
+    /// <param name="mp">
+    /// The media player.
+    /// </param>
+    /// <param name="lockCallback">
+    /// Callback to lock video memory (must not be NULL)
+    /// </param>
+    /// <param name="unlockCallback">
+    /// Callback to unlock video memory (or NULL if not needed)
+    /// </param>
+    /// <param name="displayCallback">
+    /// Callback to display video (or NULL if not needed)
+    /// </param>
+    /// <param name="userData">
+    /// Private pointer for the three callbacks (as first parameter)
+    /// </param>
+    [LibVlcFunction("libvlc_video_set_callbacks")]
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    internal delegate void SetVideoCallbacks(IntPtr mp, LockVideoCallback lockCallback, UnlockVideoCallback unlockCallback, DisplayVideoCallback displayCallback, IntPtr userData);
+}
