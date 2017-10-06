74385e14be176e7cb791505b7b5617e8e9734b06
Jeremy Vignelles <jeremy.vignelles@dev3i.fr>
2017-10-06 09:45:32 +0200

    Removed version from AssemblyInfo and updated copyright date

--- src/Vlc.DotNet.Core/Properties/AssemblyInfo.Common.cs
+++ src/Vlc.DotNet.Core/Properties/AssemblyInfo.Common.cs
@@ -3,9 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Vlc.DotNet")]
-[assembly: AssemblyCopyright("Copyright © Vlc.DotNet 2014")]
+[assembly: AssemblyCopyright("Copyright © Vlc.DotNet 2014-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2014.10.15.0")]
-[assembly: AssemblyFileVersion("2014.10.15.0")]
