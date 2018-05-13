dec0dbc59a5b9268d1b6974f5330bde41018cfe8
Jérémy VIGNELLES <jeremy.vignelles@gmail.com>
2018-05-13 18:01:59 +0200

    Moved storage of WPF callbacks references. Fixes #397

--- src/Vlc.DotNet.Core.Interops/VlcManager.SetVideoFormatCallbacks.cs
+++ src/Vlc.DotNet.Core.Interops/VlcManager.SetVideoFormatCallbacks.cs
@@ -5,18 +5,13 @@ namespace Vlc.DotNet.Core.Interops
 {
     public sealed partial class VlcManager
     {
-        private VideoFormatCallback _videoFormatCallbackReference;
-        private CleanupVideoCallback _cleanupCallbackReference;
 
         public void SetVideoFormatCallbacks(VlcMediaPlayerInstance mediaPlayerInstance, VideoFormatCallback videoFormatCallback, CleanupVideoCallback cleanupCallback)
         {
             if (mediaPlayerInstance == IntPtr.Zero)
                 throw new ArgumentException("Media player instance is not initialized.");
 
-            this._videoFormatCallbackReference = videoFormatCallback;
-            this._cleanupCallbackReference = cleanupCallback;
-
-            GetInteropDelegate<SetVideoFormatCallbacks>().Invoke(mediaPlayerInstance, this._videoFormatCallbackReference, this._cleanupCallbackReference);
+            GetInteropDelegate<SetVideoFormatCallbacks>().Invoke(mediaPlayerInstance, videoFormatCallback, cleanupCallback);
         }
     }
 }
