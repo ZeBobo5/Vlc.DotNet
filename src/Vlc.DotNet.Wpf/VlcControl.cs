807ecf2d5f0ea4532ab1ce197430edd67fdf7555
Максим Багрянцев <max.krofton@yandex.ru>
2018-07-03 00:06:19 +0300

    Changing Backgorund color of Wpf.VlcControl when updating IsAlphaChannelEnabled property

--- src/Vlc.DotNet.Wpf/VlcControl.cs
+++ src/Vlc.DotNet.Wpf/VlcControl.cs
@@ -12,6 +12,11 @@ namespace Vlc.DotNet.Wpf
     public class VlcControl: UserControl, IDisposable
     {
         /// <summary>
+        /// Stores the previous <see cref="Control.Background"/> brush after switching IsAlphaChannelEnabled to true
+        /// </summary>
+        private Brush previousBrush;
+
+        /// <summary>
         /// The Viewbox that contains the video image
         /// </summary>
         private Viewbox viewBox;
@@ -27,6 +32,22 @@ namespace Vlc.DotNet.Wpf
         private readonly VlcVideoSourceProvider sourceProvider;
 
         public VlcVideoSourceProvider SourceProvider => sourceProvider;
+
+        /// <summary>
+        /// Defines if <see cref="VlcVideoSourceProvider.VideoSource"/> pixel format is <see cref="PixelFormats.Bgr32"/> or <see cref="PixelFormats.Bgra32"/>
+        /// </summary>
+        public bool IsAlphaChannelEnabled
+        {
+            get
+            {
+                return sourceProvider.IsAlphaChannelEnabled;
+            }
+
+            set
+            {
+                sourceProvider.IsAlphaChannelEnabled = value;
+            }
+        }
         
         /// <summary>
         /// The constructor
@@ -44,12 +65,31 @@ namespace Vlc.DotNet.Wpf
             this.Background = Brushes.Black;
             // Binds the VideoSource to the Image.Source property
             this.videoContent.SetBinding(Image.SourceProperty, new Binding(nameof(VlcVideoSourceProvider.VideoSource)) { Source = sourceProvider });
+            //Listen PropertyChanged event to define if IsAlphaChannelEnabled is true or false
+            this.sourceProvider.PropertyChanged += SourceProvider_PropertyChanged;
         }
 
         /// <inheritdoc />
         public void Dispose()
         {
             sourceProvider.Dispose();
+        }
+
+        private void SourceProvider_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == nameof(VlcVideoSourceProvider.IsAlphaChannelEnabled))
+            {
+                if(this.sourceProvider.IsAlphaChannelEnabled)
+                {
+                    previousBrush = Background;
+                    Background = Brushes.Transparent;
+                }
+                else
+                {
+                    var _previousBrush = previousBrush == null ? Brushes.Black : previousBrush;
+                    Background = _previousBrush;
+                }
+            }
         }
     }
 }
