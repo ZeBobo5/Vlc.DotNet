d3f1a77c453f65455d49b466ee1438a8f90331d3
Jérémy VIGNELLES <jeremy.vignelles@gmail.com>
2018-11-11 20:28:49 +0100

    Added SetUserAgent and SetAppId

---
+++ src/Vlc.DotNet.Core.Interops/Signatures/libvlc.h/libvlc_set_app_id.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Vlc.DotNet.Core.Interops.Signatures
+{
+    /// <summary>
+    /// Sets some meta-information about the application. 
+    /// </summary>
+    /// <seealso cref="SetUserAgent" />
+    /// <param name="instance">LibVLC instance</param>
+    /// <param name="id">Java-style application identifier, e.g. "com.acme.foobar"</param>
+    /// <param name="version">application version numbers, e.g. "1.2.3"</param>
+    /// <param name="icon">application icon name, e.g. "foobar"</param>
+    [LibVlcFunction("libvlc_set_app_id")]
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    internal delegate void SetAppId(IntPtr instance, Utf8StringHandle id, Utf8StringHandle version, Utf8StringHandle icon);
+}
---
+++ src/Vlc.DotNet.Core.Interops/Signatures/libvlc.h/libvlc_set_user_agent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Vlc.DotNet.Core.Interops.Signatures
+{
+    /// <summary>
+    /// Sets the application name.
+    /// LibVLC passes this as the user agent string when a protocol requires it.
+    /// </summary>
+    /// <param name="instance">LibVLC instance</param>
+    /// <param name="name">human-readable application name, e.g. "FooBar player 1.2.3"</param>
+    /// <param name="http">HTTP User Agent, e.g. "FooBar/1.2.3 Python/2.6.0"</param>
+    [LibVlcFunction("libvlc_set_user_agent")]
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    internal delegate void SetUserAgent(IntPtr instance, Utf8StringHandle name, Utf8StringHandle http);
+}
