ad0cf7fc47458dd976ff2eec44551590a07571f4
Martin Finkel <martin@videolabs.io>
2018-11-21 11:11:41 +0700

    Fix libvlc_event_detach delegate return type

--- src/Vlc.DotNet.Core.Interops/Signatures/libvlc.h/libvlc_event_detach.cs
+++ src/Vlc.DotNet.Core.Interops/Signatures/libvlc.h/libvlc_event_detach.cs
@@ -10,8 +10,7 @@ namespace Vlc.DotNet.Core.Interops.Signatures
     /// <param name="eventType">The desired event to which we want to listen.</param>
     /// <param name="callback">The function to call when i_event_type occurs.</param>
     /// <param name="userData">User provided data to carry with the event.</param>
-    /// <returns>Return 0 on success, ENOMEM on error.</returns>
     [LibVlcFunction("libvlc_event_detach")]
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate int DetachEvent(IntPtr eventManagerInstance, EventTypes eventType, EventCallback callback, IntPtr userData);
+    public delegate void DetachEvent(IntPtr eventManagerInstance, EventTypes eventType, EventCallback callback, IntPtr userData);
 }
